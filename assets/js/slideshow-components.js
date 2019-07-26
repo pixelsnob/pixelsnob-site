f51c454231b40f76a346b67b6d619576d03c17e5
Luis A. Echeverria <pixelsnob@gmail.com>
2019-07-25 19:44:53 -0700

    Overlay close link

--- assets/js/slideshow-components.js
+++ assets/js/slideshow-components.js
@@ -22,22 +22,23 @@ class SiteOverlay extends HTMLElement {
   }
 
   connectedCallback() {
+    
+
     document.addEventListener('site-overlay-show', evt => {
       this.classList.add('site-overlay-visible');  
-      if (evt.detail.template && !this.childNodes.length) {
-        this.appendChild(evt.detail.template);
+      const siteOverlayContent = this.querySelector('.site-overlay-content');
+      if (evt.detail.template && siteOverlayContent && !siteOverlayContent.childElementCount) {
+        siteOverlayContent.appendChild(evt.detail.template);
       }
+      // Close and remove content
+      this.querySelector('.site-overlay-close').addEventListener('click', ev => {
+        this.classList.remove('site-overlay-visible');
+        document.dispatchEvent(new CustomEvent('site-overlay-hide'));
+      });
     });
     
-    // Close and remove content
-    this.addEventListener('click', ev => {
-      this.classList.remove('site-overlay-visible');
-      document.dispatchEvent(new CustomEvent('site-overlay-hide'));
-    });
   }
 }
-
-////////////////////////////////////////////////////////////////////
 
 class SlideshowPhotos extends HTMLElement {
   
@@ -55,6 +56,19 @@ class SlideshowPhotos extends HTMLElement {
     document.addEventListener('slideshow-photos-show', evt => {
       this.setAttribute('current-id', evt.detail.id);
     });
+
+    document.addEventListener('slideshow-photos-show-next', evt => {
+      const current = this.querySelector(`slideshow-photo[data-id="${evt.detail.id}"]`);
+      if (current) {
+        if (!current.dataset.listIndex) {
+          return null;
+        }
+        const next = this.querySelector(`slideshow-photo[data-list-index="${Number(current.dataset.listIndex) + 1}"]`);
+        if (next) {
+          this.setAttribute('current-id', next.dataset.id);
+        }
+      }
+    });
   }
 
   static get observedAttributes() {
@@ -70,6 +84,7 @@ class SlideshowPhotos extends HTMLElement {
       case 'current-id':
         const current = this.querySelector(`slideshow-photo[data-id="${newVal}"]`);
         if (current) {
+          // Move the entire container to the left/right so it shows current image
           const i = current.getAttribute('data-list-index');
           this.style.left = `calc(-${i} * 100vw)`;
           document.dispatchEvent(new CustomEvent('slideshow-photo-show', {
@@ -79,8 +94,6 @@ class SlideshowPhotos extends HTMLElement {
       break;
     }
   }
-
-  // set position here
 }
 
 class SlideshowPhoto extends HTMLElement {
@@ -90,6 +103,14 @@ class SlideshowPhoto extends HTMLElement {
   }
 
   connectedCallback() {
+    this.addEventListener('click', evt => {
+      document.dispatchEvent(new CustomEvent('slideshow-photo-click', {
+        detail: { id: this.dataset.id, listIndex: this.dataset.listIndex }
+      }));
+      evt.preventDefault();
+      evt.stopPropagation();
+    });
+
     document.addEventListener('slideshow-photo-show', evt => {
       if (this.dataset.id === evt.detail.id) {
         this.classList.add('photo-visible');
