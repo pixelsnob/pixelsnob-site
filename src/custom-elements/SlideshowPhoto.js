8645312b0f736e6150e69b7f79a44d2a36d880d6
Luis A. Echeverria <pixelsnob@gmail.com>
2019-08-01 13:24:38 -0700

    Disable prefetch

--- src/custom-elements/SlideshowPhoto.js
+++ src/custom-elements/SlideshowPhoto.js
@@ -28,7 +28,7 @@ export default class SlideshowPhoto extends HTMLElement {
       if (isImageLoaded) {
         existingImg.src = existingImg.dataset.src;
         this.className = 'photo-visible';
-        this.prefetch(slideshowPhotoId);
+        //this.prefetch(slideshowPhotoId);
       } else {
         const tmpImg = new Image;
         tmpImg.onload = evt => {
@@ -40,7 +40,7 @@ export default class SlideshowPhoto extends HTMLElement {
           }
           // Add to image cache
           store.dispatch(setImageLoaded(existingImg.dataset.src));
-          this.prefetch(slideshowPhotoId);
+          //this.prefetch(slideshowPhotoId);
         };
         tmpImg.src = existingImg.dataset.src;
         existingImg.src = tmpImg.src;
