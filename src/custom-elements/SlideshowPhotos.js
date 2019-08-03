0c90a87ddcea746e86f1ab20bb27a03450cbd96e
Luis A. Echeverria <pixelsnob@gmail.com>
2019-08-02 20:06:05 -0700

    Cleanup

--- src/custom-elements/SlideshowPhotos.js
+++ src/custom-elements/SlideshowPhotos.js
@@ -1,14 +1,9 @@
 import store from '../store';
-import createObserver from '../createObserver';
 
 export default class SlideshowPhotos extends HTMLElement {
 
   constructor() {
     super();
-    console.log('sdasd')
-    const loadingProgress = document.createElement('loading-progress');
-    //loadingProgress.classList.add('loading-progress');
-    this.appendChild(loadingProgress);
   }
 
   connectedCallback() {
@@ -19,22 +14,5 @@ export default class SlideshowPhotos extends HTMLElement {
       this.appendChild($photo);
       $photo.setAttribute('data-id', photo.id);
     });
-    // this._storeUnsubscribe = createObserver(store)(
-    //   state => ({ slideshowPhotoId: state.slideshowPhotoId }),
-    //   (slideshowPhotoId) => {
-    //     if (slideshowPhotoId) {
-    //       //this.showLoadingProgress();
-    //       //this.querySelector('.loading-progress')
-    //     }
-    //     //console.log('dsal')
-    //     // if (overlayShow) {
-    //     //   this.classList.add('site-overlay-visible');
-    //     //   document.body.classList.add('no-scroll');
-    //     // } else {
-    //     //   document.body.classList.remove('no-scroll');
-    //     //   this.classList.remove('site-overlay-visible');
-    //     // }
-    //   }
-    // );
   }
 }
