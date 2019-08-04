ee8739eff06ca23222746888989fbf61fb8874a3
Luis A. Echeverria <pixelsnob@gmail.com>
2019-08-03 22:57:28 -0700

    Click on progress bar to load an image based on position in list

--- src/custom-elements/SlideshowProgress.js
+++ src/custom-elements/SlideshowProgress.js
@@ -1,38 +1,60 @@
 
 import store from '../store';
 import createObserver from '../createObserver';
+import { setSlideshowPhotoId } from '../actions';
 
 export default class SlideshowProgress extends HTMLElement {
 
   constructor() {
     super();
-    this._progress = document.createElement('div');
-    this._progress.className = 'slideshow-progress-bar';
-    this.appendChild(this._progress);
+    this._progressBar = document.createElement('div');
+    this._progressBar.className = 'slideshow-progress-bar';
+    this.addEventListener('click', this.onClick.bind(this));
+    this.appendChild(this._progressBar);
+    
+    
   }
   connectedCallback() {
     this._storeUnsubscribe = createObserver(store)(
       state => ({ slideshowPhotoId: state.slideshowPhotoId, slideshowPhotos: state.slideshowPhotos }),
       (key, value) => {
         if (key === 'slideshowPhotoId') {
-          const slideshowPhotos = store.getState().slideshowPhotos;
-          if (slideshowPhotos) {
-            const photo = slideshowPhotos.find(photo => photo.id === value);
-            if (photo) {
-              const percent = (photo.listIndex / (slideshowPhotos.length - 1)) * 100;
-              this._progress.style.width = percent + '%';
-            }
-            return null;
-          }
+          this.updateProgressBar(value);
         }
-        this._progress.style.width = '0';
       }
     );
+  }
+
+  updateProgressBar(slideshowPhotoId) {
+    this._progressBar.style.width = '0';
+    const slideshowPhotos = store.getState().slideshowPhotos;
+    if (slideshowPhotos) {
+
+      const photo = slideshowPhotos.find(photo => photo.id === slideshowPhotoId);
+      if (photo) {
+        const percent = (photo.listIndex / (slideshowPhotos.length - 1)) * 100;
+        this._progressBar.style.width = percent + '%';
+      }
+    }
+  }
+
+  onClick(ev) {
+    ev.preventDefault();
+    const state = store.getState();
+    if (state.slideshowPhotos) {
+      // Load a photo by list index, based on where the progress bar was clicked.
+      const listIndex = Math.ceil((ev.clientX / window.innerWidth) * (state.slideshowPhotos.length - 1));
+      const photo = state.slideshowPhotos.find(photo => photo.listIndex === listIndex);
+      if (photo) {
+        store.dispatch(setSlideshowPhotoId(photo.id));
+      }
+    }
   }
 
   disconnectedCallback() {
     if (this._storeUnsubscribe) {
       this._storeUnsubscribe();
     }
+    this._progressBar.removeEventListener('click', this.onClick.bind(this));
   }
 }
