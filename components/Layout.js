7868f4ce213ae913d6f8164a2457ddee86f2bc0e
Luis A. Echeverria <pixelsnob@gmail.com>
2020-06-23 16:51:04 -0700

    Cleanup

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,22 @@
 
 export default props => (
-  <>
-    <div className="main-container">
-      
-      <section className="main-content-container">
-        <header className="main-header">
-          <h1 className="">
-            <span className="rainbow-shadowbox">
-              <a href="/" className="main-home-link rainbow">pixelsnob.com</a>
-            </span>
-          </h1>
-        </header>
-        
-        <div className="main-content">{props.children}</div>
-      </section>
-      <footer>
-        
-      </footer>
-    </div>
 
-  </> 
+  <div className="main-container">
+
+    <section className="main-content-container">
+      <header className="main-header">
+        <h1 className="">
+          <span className="rainbow-shadowbox">
+            <a href="/" className="main-home-link rainbow">pixelsnob.com</a>
+          </span>
+        </h1>
+      </header>
+
+      <div className="main-content">{props.children}</div>
+    </section>
+    <footer>
+
+    </footer>
+  </div>
+
 )
